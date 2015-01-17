ce45de079d21036708b3513fff93ce0fb31ce0b8
Peter Lyons <lyoncub1028@gmail.com>
2015-01-17 03:23:51 -0500

    Initial Commit

---
+++ NewsTicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsTicker
+//
+//  Created by Peter Lyons on 1/17/15.
+//  Copyright (c) 2015 LyonCub Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsTicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsTicker
+//
+//  Created by Peter Lyons on 1/17/15.
+//  Copyright (c) 2015 LyonCub Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
